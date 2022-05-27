f1d4cf811daa938e17d204059501fa2b3fbc9312
Mark <formarkv@gmail.com>
2022-05-27 17:38:05 -0500

    started dictionary parsing. Got up to DDD Offset. Need to implement a loop to read through all DDD entries for multi mesh

--- SMFmark.py
+++ SMFmark.py
@@ -57,9 +57,9 @@ def ReadVector(fileobject):
     return Vector([x,y,z])
 
 def ReadFaceIndex(fileobject):
-    fileobject.seek(112144,0) 
+    fileobject.seek(210388,0) 
     faceindexlist = []
-    for i in range(500): #this is not supposed to be same as floats
+    for i in range(4100): #this is not supposed to be same as floats
         faceindextriple = ReadShortTriple(fileobject)
         #print(faceindextriple)
         faceindexlist.append(faceindextriple)
@@ -76,13 +76,13 @@ def ImportModel(fileobject, offsettomodel, ModelType):
         seekvalue = 28
     if ModelType == 0:
         seekvalue = 12
-    for i in range(300): 
+    for i in range(4650): 
         vertex = ReadVector(fileobject)
         vertexlist.append(vertex)
         fileobject.seek(seekvalue,1)
-    print(vertexlist)
+    #print(vertexlist)
     faceindexlist = ReadFaceIndex(fileobject)
-    print(faceindexlist)
+    #print(faceindexlist)
     
     mesh = bpy.data.meshes.new("Cod_Vita_Mesh")
     mesh.from_pydata(vertexlist,[],faceindexlist)
@@ -91,14 +91,17 @@ def ImportModel(fileobject, offsettomodel, ModelType):
     scene = bpy.context.scene
     scene.collection.objects.link(object)
 
-def GetModelOffset(fileobject):
-    fileobject.seek(12,0) #reading end of the file directory so we can jmp from it
-    EndOfFileDirectoryOffsetint = ReadInt32(fileobject)
+def GetModelOffset(fileobject, EndofFileDirectoryOffset):
+    ###If you want to be slightly more official, you could go to the 
+    ###second nsirsrc string section (do this by going to offsets at end of first string section)
+    ###find 31 byte and seek 8 bytes to the overall offset of FFFF7FFF
+    
     fileobject.seek(256,0) #looking for FFFF7FFF for other offsets
     FFBYTECONFIRM = 0
     #FFBYTE = b'\xff\xff\x7f\xff' \xff\x7f\xff
+    
     i=0
-    for i in range (200):
+    for i in range (50):
         
         FFBYTE = fileobject.read(1)
         if FFBYTE == b'\xff':
@@ -106,35 +109,64 @@ def GetModelOffset(fileobject):
             print(FFBYTECONFIRM)
             break
             
-        if i == 199: 
+        if i == 49: 
             
             print("byte not found, returning")
             return          
 
     VerticesAndFacesSizeint = ReadInt32(fileobject) #after FFFF7FFF we have vertices+faces region size
     OffsetFromDirectoryToModelint =  ReadInt32(fileobject) #the offset to jmp from end of directory to model
-    TotalOffsetToModel = EndOfFileDirectoryOffsetint + OffsetFromDirectoryToModelint
-    print(VerticesAndFacesSizeint)
-    print(TotalOffsetToModel)
+    TotalOffsetToModel = EndOfFileDirectoryOffset + OffsetFromDirectoryToModelint
+    #print(VerticesAndFacesSizeint)
+    #print(TotalOffsetToModel)
     return TotalOffsetToModel       
+
+def FourBytesAreNonZero(fileobject):
+    for i in range (4):
+        readbyte = fileobject.read(1)
+        if readbyte == 0:
+            return False
+        else:
+            continue
+
+    return True
+
+
+
+
+
+def GetDicData(fileobject, EndOfFileDirectoryOffset):
+    ### Pookey chicken method. we are going seek 158 hex past the end of nsirsrc header
+    ###then we are going to search for what I believe is the model tag id
+    ###then you take the next 4 bytes as the offset to the real dictionary. 
+    ###If those 4 bytes are 0, you take the NEXT 4 and that is usually the dictionary offset 
+    fileobject.seek(EndOfFileDirectoryOffset,0)
+    fileobject.seek(344,1)
+    OffsetToFirstDicbase = 0
+    while OffsetToFirstDicbase == 0:
+        if FourBytesAreNonZero(fileobject):
+            OffsetToFirstDicbase = ReadInt32(fileobject)
+            if OffsetToFirstDicbase == 0:
+               OffsetToFirstDicbase = ReadInt32(fileobject)
+    print(OffsetToFirstDicbase)
+    FirstdicOffset = OffsetToFirstDicbase + EndOfFileDirectoryOffset  
+    fileobject.seek(FirstdicOffset + 24, 0) #moving to 1st dic + 18 hex
+    OffsetToDDDDirectorybase = ReadInt32(fileobject) #reading 400 from the 1st dic
+    DDDDirectoryOffset = OffsetToDDDDirectorybase + EndOfFileDirectoryOffset  #adding the 400 to the string end of header
 
 
 
       
 
     
-        
-
-    
-        
-
-
-
 def ReadDataFromFile(context, filepath):
     fileobjectsmf = open(filepath, "rb")
     #print(fileobjectsmf.read(5))
-    ModelTypeCurrent = 0 #this will need to be changed based off file
-    OffsetToModel = GetModelOffset(fileobjectsmf)
+    ModelTypeCurrent = 2 #this will need to be changed based off file
+    fileobjectsmf.seek(12,0) #reading end of the file directory so we can jmp from it
+    EndOfNsiFileDirectoryOffsetint = ReadInt32(fileobjectsmf)
+    #GetDicData(fileobjectssmf, EnfOfNsiFileDirectoryOffsetint)
+    OffsetToModel = GetModelOffset(fileobjectsmf, EndOfNsiFileDirectoryOffsetint )
     ImportModel(fileobjectsmf, OffsetToModel, ModelTypeCurrent)
     return
     
